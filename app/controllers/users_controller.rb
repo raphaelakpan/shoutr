e93d28599e5f5dd7e4c72b9567c62bdeaf224f06
Raphael Akpan <raphael.akpan@andela.com>
2019-07-14 14:13:12 +0100

    Fix null db values

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < Clearance::UsersController
 
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
@@ -8,7 +12,6 @@ class UsersController < Clearance::UsersController
   private
 
   def user_params
-    return Hash.new if params[:action] == 'new'
     params.require(:user).permit(:username, :email, :password)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_14_112417) do
+ActiveRecord::Schema.define(version: 2019_07_14_125708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id"
     t.index ["user_id"], name: "index_shouts_on_user_id"
   end
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_07_14_112417) do
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
-    t.string "username"
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
